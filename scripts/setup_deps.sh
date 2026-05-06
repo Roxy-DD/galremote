e6259f7c5e41c8c7f8ba4b976166dc01561bb07d
Roxy-DD <2176891692@qq.com>
2026-05-07 04:56:16 +0800

    fix: remove msys2 cmake to fallback to native cmake and avoid 127 error

--- scripts/setup_deps.sh
+++ scripts/setup_deps.sh
@@ -16,5 +16,5 @@ if [ -n "$tarballs" ]; then
   pacman -U --noconfirm ${tarballs}
 fi
 
-dependencies=("git" "mingw-w64-ucrt-x86_64-cmake" "mingw-w64-ucrt-x86_64-ninja" "mingw-w64-ucrt-x86_64-cppwinrt" "mingw-w64-ucrt-x86_64-curl-winssl" "mingw-w64-ucrt-x86_64-graphviz" "mingw-w64-ucrt-x86_64-MinHook" "mingw-w64-ucrt-x86_64-miniupnpc" "mingw-w64-ucrt-x86_64-nlohmann-json" "mingw-w64-ucrt-x86_64-nodejs" "mingw-w64-ucrt-x86_64-nsis" "mingw-w64-ucrt-x86_64-onevpl" "mingw-w64-ucrt-x86_64-openssl" "mingw-w64-ucrt-x86_64-opus" "mingw-w64-ucrt-x86_64-toolchain")
+dependencies=("git" "mingw-w64-ucrt-x86_64-ninja" "mingw-w64-ucrt-x86_64-cppwinrt" "mingw-w64-ucrt-x86_64-curl-winssl" "mingw-w64-ucrt-x86_64-graphviz" "mingw-w64-ucrt-x86_64-MinHook" "mingw-w64-ucrt-x86_64-miniupnpc" "mingw-w64-ucrt-x86_64-nlohmann-json" "mingw-w64-ucrt-x86_64-nodejs" "mingw-w64-ucrt-x86_64-nsis" "mingw-w64-ucrt-x86_64-onevpl" "mingw-w64-ucrt-x86_64-openssl" "mingw-w64-ucrt-x86_64-opus" "mingw-w64-ucrt-x86_64-toolchain")
 pacman -Syu --noconfirm --ignore="$(IFS=,; echo "${broken_deps[*]}")" "${dependencies[@]}"
