e846ed88eb8a557b9c226757efa7a93baea9838b
Roxy-DD <2176891692@qq.com>
2026-05-07 06:14:36 +0800

    fix: undefine MinGW SAL macros in driver_settings.h to allow NVAPI headers to fallback to empty macros, preventing GCC compile errors

--- src/platform/windows/nvprefs/driver_settings.h
+++ src/platform/windows/nvprefs/driver_settings.h
@@ -8,6 +8,28 @@
 // disable clang-format header reordering
 // as <NvApiDriverSettings.h> needs types from <nvapi.h>
 // clang-format off
+// Undefine MinGW SAL macros so nvapi_lite_salstart.h can correctly redefine them to empty
+#ifdef __GNUC__
+#undef __in
+#undef __in_bcount
+#undef __in_ecount
+#undef __in_opt
+#undef __in_ecount_opt
+#undef __out
+#undef __out_bcount
+#undef __out_ecount
+#undef __out_opt
+#undef __out_ecount_full_opt
+#undef __out_ecount_part_opt
+#undef __inout
+#undef __inout_bcount
+#undef __inout_ecount
+#undef __inout_opt
+#undef __inout_ecount_part_opt
+#undef __inout_ecount_full
+#undef __inout_ecount_full_opt
+#endif
+
 #include <nvapi.h>
 #include <NvApiDriverSettings.h>
 // clang-format on
